1caa6d4f61dc6c62cb5e1fb23a1dae53ae5f5a2e
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2024-08-08 14:14:46 -0300

    Actualizando endpoint de back

--- src/helpers/Global.js
+++ src/helpers/Global.js
@@ -1,12 +1,12 @@
 export const Global = {
   endpoints: {
-    backend: "http://localhost:8080/api/",
+    backend: "http://52.91.157.108:8080/api/",
   },
   actionType: {
     SET_CLIENTS: "SET_CLIENTS",
     SET_LOADING: "SET_LOADING",
   },
   images: {
-    logo: "Logo MiAlarma.png"
-  }
+    logo: "Logo MiAlarma.png",
+  },
 };
